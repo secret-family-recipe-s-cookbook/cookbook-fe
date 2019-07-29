f1748f4204248f6ece432988bf51397e302db3d6
Jengopockets <jengopockets@gmail.com>
2019-07-29 18:16:33 -0400

    merge

--- secret_recipies/src/components/Pages/PrivacyPolicy.js
+++ secret_recipies/src/components/Pages/PrivacyPolicy.js
@@ -4,9 +4,9 @@ function PrivacyPolicy () {
     return(
         <div className='privacy-page'>
             <h1>Privacy Policy of Secret Family Recipes Cookbook</h1>
-            <p>Company Name operates the Website Name website, which provides the SERVICE.<br>
-            This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Website Name website.<br>
-            If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.<br>
+            <p>Company Name operates the Website Name website, which provides the SERVICE.<br /><br />
+            This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Website Name website.<br /><br />
+            If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.<br /><br />
             The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Website URL, unless otherwise defined in this Privacy Policy.</p>
         </div>
     )
