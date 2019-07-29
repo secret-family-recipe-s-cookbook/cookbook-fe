b5885a358d6149a423072259e85c159d69914dde
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-07-29 17:06:15 -0500

    working on Modal functionality

--- secret_recipies/src/components/Login/Login.js
+++ secret_recipies/src/components/Login/Login.js
@@ -1,41 +1,50 @@
-import React, {useState} from "react";
-//import { Button, Modal } from 'semantic-ui-react'
+import React,{useState} from "react";
 
 
-const Login = (props) => {
-    const [show, setShow] = useState(false);
-    const [email, setEmail]= useState()
-    const [password, setPassword] = useState()
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
- 
-    return (
-        <div className="login-container">
-           {/* <Modal
-                trigger={<Button>Show Modal</Button>}
-                header='Reminder!'
-                content='Call Benjamin regarding the reports.'
-                actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
-            /> */}
-            
-        </div>
-    )
-}
+export default function Modal(props) {
+  const [name, setName] = useState({
+    username: "",
+    password: ""
+  });
 
-export default Login
+  // TASK: Add handler(s) to print form field on submit!
+  // STRETCH: Add multiple inputs w/ a single handler
 
-   {/* <form >
-                <h5>Sign In</h5>
-                <div className="input-field">
-                    <label>
-                        Email
-                        <input type="email" name="email" />
-                    </label>
-                    <label>
-                        Password
-                        <input type="password" name="password" />
-                    </label>
-                    <button>Login</button>
-                </div>
-            </form> */}
+  const changeHandler = event => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setName({ ...name, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(name);
+  };
+  return (
+    <div >
+      <form className="login-modal" onSubmit={event => handleSubmit(event)}>
+        <label>
+          Username:
+          <input className ="input-modal"
+            type="text"
+            name="username"
+            onChange={changeHandler}
+            value={name.username}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input className ="input-modal"
+            type="password"
+            name="password"
+            onChange={changeHandler}
+            value={name.password}
+          />
+        </label>
+
+        <button className="button-modal">Submit!</button>
+      </form>
+    </div>
+  );}
--- secret_recipies/src/components/NavBar.js
+++ secret_recipies/src/components/NavBar.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 //import { Link } from "react-router-dom"
 
-//import Login from "./Login/Login"
+
+import Login from "./Login/Login"
+import "./Login/Login.css"
+
 
 function Navbar () {
+    const [isModalVisible, setIsModalVisible] = useState(false)
     return (
-        <div className='navbar sticky'>
+          <div className='navbar sticky'>
             {/* Logo will be placed first  */}
             <h1 className='title'>Secret Family Recipe Cookbook</h1>
-           <h1 className='signIn'>Sign In</h1>
+            <button onClick={()=>setIsModalVisible(!isModalVisible)}>Sign In</button>
+            {isModalVisible && (
+                <Login onModalClose={()=> setIsModalVisible(false)}/>
+            )}
             <h1 className='signIn'>Join</h1>
         </div>
     )
 }
 
+
+
 export default Navbar;
+
+//<h1 className='signIn'>Sign In</h1>
